f8f0fd01fc02583c2aef0ca40431206602d3c6e1
gehuaihui <1178436805@qq.com>
2019-06-17 17:48:19 +0800

    first commit

---
+++ src/views/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Com extends React.Component{
+  render () {
+    return (
+      <div className="box">
+        <header className="header">用户中心头部</header>
+        <div className="content">用户中心内容</div>
+      </div>
+    )
+  }
+}
+export default Com;
